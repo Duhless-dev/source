dd9bf7b9bfc50eb544c798e8876a84fb2335bcb2
Никита Кувашов <nikitakuvasov@MacBook-Air-Nikita.local>
2021-01-02 13:20:23 +0300

    add forms

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,10 @@
 import "./slider";
 import modal from "./modules/modal";
 import tabs from "./modules/tabs";
+import forms from "./modules/forms";
 document.addEventListener("DOMContentLoaded",()=>{
     modal();
     tabs(".decoration_content>div>div",".no_click",".decoration_slider",".after_click");
     tabs(".glazing_content",".glazing_block",".glazing_slider",".active");
+    forms();
 });
